060f49cc4ab0ebe2c1862d4db974f6fcee3d2de7
cesarroba <46606931+cesarroba@users.noreply.github.com>
2019-01-22 23:37:50 +0100

    Firtst commit
    Error read text

---
+++ SudokuBOT/src/Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate {
+    public void BCalculate (int numbers[][]) { // [] vertical [][] horizontal
+        
+        for (int i = 1; i < 10; i++) { // vertical
+            for (int j = 1; j < 10; j++) { // horizontal
+                
+            }
+        }
+    }
+}
